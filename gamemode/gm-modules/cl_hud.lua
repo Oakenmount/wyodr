46a40315972cba19e21d934d84562aacfa277a3c
DediUpdater <aupd@notexist.github.com>
2014-03-05 16:00:01 +0100

    auto update 2014-03-05 16:00:01

--- gamemode/gm-modules/cl_hud.lua
+++ gamemode/gm-modules/cl_hud.lua
@@ -2,20 +2,32 @@ local useless_stuff = {"CHudHealth", "CHudBattery", "CHudAmmo", "CHudSecondaryAm
 
 hook.Add("HUDShouldDraw", "HideEverythingUseless", function(name)
     if GetConVarNumber("cl_hudstyle") == 4 then return end
-if table.HasValue(useless_stuff, name) then return false end
+    if table.HasValue(useless_stuff, name) then return false end
 end)
+
+local rstate_colors = {
+    [ROUND_WAIT] = Color(149, 165, 166),
+    [ROUND_ACTIVE] = Color(155, 89, 182),
+    [ROUND_POST] = Color(39, 174, 96)
+}
+
+local rstate_names = {
+    [ROUND_WAIT] = "Waiting/Preparing",
+    [ROUND_ACTIVE] = "Active",
+    [ROUND_POST] = "Post-round"
+}
 
 hook.Add("HUDPaint", "YouGottaDrawTheHUD", function()
     local rstate = wyodr.GetRoundState()
-    surface.SetDrawColor(255, 255, 255)
-    surface.DrawRect(0, 0, ScrW(), 25)
     
-    draw.SimpleText(tostring(wyodr.GetRoundState()), "DermaLarge", 100, 100)  
-        
     local roundProgress = (CurTime() - GetGlobalFloat("roundstart"))
     local roundEnd = (GetGlobalFloat("roundend") - CurTime())
         
-    draw.SimpleText(string.ToMinutesSeconds(math.Round(roundEnd)), "DermaLarge", 100, 130)   
+    local roundfrac = roundProgress / wyodr.RoundLengths[rstate]
+    surface.SetDrawColor(rstate_colors[rstate])
+    surface.DrawRect(0, 0, ScrW()*roundfrac, 25)
+    
+    draw.SimpleText(rstate_names[rstate] .. " " .. string.ToMinutesSeconds(math.Round(roundEnd)), "DermaLarge", 3, -2, Color(255, 255, 255))   
 end)
 
 hook.Add("CalcView", "DeathView", function(pl, origin, angles, fov)
