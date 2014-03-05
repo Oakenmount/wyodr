f19b64ea53d9ae4ffbf0901d02ea798b7251f010
DediUpdater <aupd@notexist.github.com>
2014-03-05 18:15:01 +0100

    auto update 2014-03-05 18:15:01

--- gamemode/gm-modules/cl_bhop.lua
+++ gamemode/gm-modules/cl_bhop.lua
@@ -1,12 +1,8 @@
 hook.Add("CreateMove", "BHop", function(ucmd)
 	if IsValid(LocalPlayer()) and bit.band(ucmd:GetButtons(), IN_JUMP) > 0 then
+	    ucmd:SetButtons( ucmd:GetButtons() - IN_JUMP )
 		if LocalPlayer():OnGround() then
             ucmd:SetButtons( ucmd:GetButtons() + IN_JUMP )
-            ucmd:SetButtons( ucmd:GetButtons() - IN_JUMP )
-                if ucmd:KeyDown( IN_JUMP ) then
-                    ucmd:SetButtons( ucmd:GetButtons() )
-                end
-                
 		end
 	end
 end)
