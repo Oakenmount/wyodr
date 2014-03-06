aa19ba72173c1538ed52e0bb426fa71628dcd372
DediUpdater <aupd@notexist.github.com>
2014-03-06 15:05:01 +0100

    auto update 2014-03-06 15:05:01

--- gamemode/gm-modules/sh_teams.lua
+++ gamemode/gm-modules/sh_teams.lua
@@ -1,5 +1,24 @@
 team.SetUp(TEAM_RUNNER,"Runners",Color(192, 57, 43))
 team.SetUp(TEAM_DEATH,"Death",Color(52, 73, 94))
 team.SetUp(TEAM_SPECTATOR,"Spectator",Color(243, 156, 18))
-team.SetSpawnPoint( TEAM_RUNNER, "info_player_terrorist" )
-team.SetSpawnPoint( TEAM_DEATH, "info_player_counterterrorist" )
+team.SetSpawnPoint( TEAM_DEATH, "info_player_terrorist" )
+team.SetSpawnPoint( TEAM_RUNNER, "info_player_counterterrorist" )
+
+--[[
+hook.Add("PlayerSelectSpawn","selectleepikspawn",function( pl )
+    
+    if pl:Team() == TEAM_RUNNER then
+            local spawns = ents.FindByClass( "info_player_counterterrorist" )
+            local random_entry = math.random(#spawns)
+ 
+            return spawns[random_entry]
+    end
+    if pl:Team() == TEAM_DEATH then
+            local spawns = ents.FindByClass( "info_player_terrorist" )
+            local random_entry = math.random(#spawns)
+ 
+            return spawns[random_entry]
+    end             
+ 
+end)
+]]
