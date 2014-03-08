e85fe5183df3b7bf6c91052e3c0a816bec67d4e4
DediUpdater <aupd@notexist.github.com>
2014-03-08 17:55:01 +0100

    auto update 2014-03-08 17:55:01

--- gamemode/gm-modules/sv_ghostmode.lua
+++ gamemode/gm-modules/sv_ghostmode.lua
@@ -1,8 +1,13 @@
 hook.Add("PlayerDeath", "GhostMode", function(ply)
     timer.Simple(2, function()
         if IsValid(ply) and not ply:Alive() and ply:IsSuperAdmin() then
-            ply:Spawn()
-            ply:SetMaterial("models/props_c17/frostedglass_01a")
+            --wply:Spawn()
+            --ply:SetMaterial("models/props_c17/frostedglass_01a")
+            --ply.IsGhost = true
         end
     end)
+end)
+
+hook.Add("EntityTakeDamage", "GhostModeSwag", function(ply, dmginfo)
+    --if ply.IsGhost then dmginfo:ScaleDamage(0) end
 end)
